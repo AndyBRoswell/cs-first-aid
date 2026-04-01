60f488188406abf066acff51aaef275f5030d9c9
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2026-04-01 22:30:54 +0200

    Trying to add sidebar with Starlight

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,19 @@
 import { defineConfig } from "astro/config";
-
 import preact from "@astrojs/preact";
+
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact()]
+  integrations: [
+    preact(),
+    starlight(
+      {
+        title: 'CS First Aid',
+        sidebar: [
+          { slug: 'preface/preface' },
+        ],
+      }
+    )
+  ]
 });
