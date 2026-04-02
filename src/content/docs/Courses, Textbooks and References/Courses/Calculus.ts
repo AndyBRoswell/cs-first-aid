ad2b3173a8b3692a2678a97c0932ad27f032e85d
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2026-04-02 16:30:23 +0200

    Adding IEEE-style references

--- src/content/docs/Courses, Textbooks and References/Courses/Calculus.ts
+++ src/content/docs/Courses, Textbooks and References/Courses/Calculus.ts
@@ -1,65 +1,62 @@
-import Cite from 'citation-js'
+// import Cite from 'citation-js'
 
 import * as base from './base.ts'
-import type { Course_Material } from "./base.ts";
 
 export const info: base.Course = {
   canonical_name: '微积分',
   names: [ '微积分', '高等数学', ],
   tags: [ '基础必修', ],
-  textbooks: [
-    {
-      type: 'book',
-      id: 'Calculus: A Complete Course',
-      author: [
-        {
-          family: 'Adams',
-          given: 'Robert A.',
+  materials: {
+    textbooks: [
+      {
+        type: 'book',
+        id: 'Calculus: A Complete Course',
+        author: [
+          {
+            family: 'Adams',
+            given: 'Robert A.',
+          },
+          {
+            family: 'Essex',
+            given: 'Christopher',
+          },
+        ],
+        title: 'Calculus: A Complete Course',
+        edition: 10,
+        publisher: 'Pearson',
+        "publisher-place": 'Toronto, ON',
+        issued: {
+          "date-parts": [ [ 2021, ], ],
         },
-        {
-          family: 'Essex',
-          given: 'Christopher',
+        "number-of-pages": 1200,
+        ISBN: '9780135766781',
+        language: 'en-CA',
+        URL: 'https://www.pearson.com/en-ca/subject-catalog/p/calculus-a-complete-course/P200000002479/9780135766781',
+        note: 'Printed ver: 9780135732588',
+      },
+      {
+        type: 'book',
+        id: '简明微积分',
+        author: [
+          {
+            literal: '龚昇',
+          }
+        ],
+        title: '简明微积分',
+        edition: 4,
+        publisher: '高等教育出版社',
+        "publisher-place": '北京',
+        issued: {
+          "date-parts": [ [ 2006, ], ],
         },
-      ],
-      title: 'Calculus: A Complete Course',
-      edition: 10,
-      publisher: 'Pearson',
-      "publisher-place": 'Toronto, ON',
-      issued: {
-        "date-parts": [ [ 2021, ], ],
+        "number-of-pages": 565,
+        ISBN: '9787040186932',
+        "collection-title": "普通高等教育“十五”国家级规划教材",
+        language: "zh-CN",
+        URL: 'https://book.douban.com/subject/2025549/',
       },
-      "number-of-pages": 1200,
-      ISBN: '9780135766781',
-      language: 'en-CA',
-      URL: 'https://www.pearson.com/en-ca/subject-catalog/p/calculus-a-complete-course/P200000002479/9780135766781',
-      note: 'Printed ver: 9780135732588',
-    },
-    {
-      type: 'book',
-      id: '简明微积分',
-      author: [
-        {
-          literal: '龚昇',
-        }
-      ],
-      title: '简明微积分',
-      edition: 4,
-      publisher: '高等教育出版社',
-      "publisher-place": '北京',
-      issued: {
-        "date-parts": [ [ 2006, ], ],
-      },
-      "number-of-pages": 565,
-      ISBN: '9787040186932',
-      "collection-title": "普通高等教育“十五”国家级规划教材",
-      language: "zh-CN",
-      URL: 'https://book.douban.com/subject/2025549/',
-    },
-  ]
+    ]
+  }
 }
 
-export const References: { [key: string]: Course_Material[] } = {
-  Textbooks: new Cite(info.textbooks).format('bibliography', {
-    template: 'ieee',
-  })
-}
+export const References = base.print_bibliography(info.materials!)
