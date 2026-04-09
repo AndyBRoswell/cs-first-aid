422c928a21730189a029bbb5ef61a65268b06b00
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2026-04-10 00:40:49 +0200

    slight modifications

--- src/content/docs/Courses, Textbooks and References/Courses/Mathematical Analysis/data.ts
+++ src/content/docs/Courses, Textbooks and References/Courses/Mathematical Analysis/data.ts
@@ -2,6 +2,7 @@ import * as base from '../../base.ts'
 import * as catalog from '../../Materials/catalog.ts'
 import '../../Materials/常庚哲 史济怀 数分.ts'
 import '../../Materials/H&N Applied Analysis.ts'
+import '../../Materials/Abbott, Understanding Analysis.ts'
 import '../../Materials/刘思齐 数分选书.ts'
 import '../../Materials/分析学爱好者 我为何不建议初学者用清华丘班数分讲义学数分.ts'
 
@@ -16,6 +17,7 @@ export const info = {
       catalog.get('常庚哲 史济怀 数分 2026 上'),
       catalog.get('常庚哲 史济怀 数分 2026 下'),
       catalog.get('H&N Applied Analysis'),
+      catalog.get('Abbott, Understanding Analysis'),
     ],
     other: [
       catalog.get('刘思齐 数分选书'),
