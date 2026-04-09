d8af7976ba00cac5f4b89ce888ddd8381c4b10f7
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2026-04-10 00:40:24 +0200

    Adding course Mathematical Analysis

---
+++ src/content/docs/Courses, Textbooks and References/Materials/Abbott, Understanding Analysis.ts
@@ -0,0 +1,24 @@
+import * as catalog from './catalog.ts'
+import * as base from '../base.ts'
+
+const book = {
+  type: 'book',
+  id: 'Abbott, Understanding Analysis',
+  author: [ { given: 'Stephen', family: 'Abbott' } ],
+  title: 'Understanding Analysis',
+  edition: 2,
+  publisher: 'Springer',
+  "publisher-place": 'New York, NY',
+  issued: { "date-parts": [ [ 2015, ], ], },
+  "number-of-pages": 312,
+  ISBN: '978-1-4939-2712-8',
+  "collection-title": 'Undergraduate Texts in Mathematics',
+  "collection-editor": [ { given: 'Sheldon Jay', family: 'Axler' }, { given: 'Kenneth Alan', family: 'Ribet' }, ],
+  language: 'en-US',
+  URL: 'https://link.springer.com/book/10.1007/978-1-4939-2712-8',
+  accessed: { "date-parts": [ [ 2026, 4, 10 ], ], },
+} satisfies base.Course_Material
+
+catalog.add([
+  book.id,
+], book)
