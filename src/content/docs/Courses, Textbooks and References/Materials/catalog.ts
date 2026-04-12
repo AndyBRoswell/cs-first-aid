9799052139da568c962f46c18bdcec2cf4ee7671
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2026-04-12 15:09:34 +0200

    Fine-grained type constraints for ID

--- src/content/docs/Courses, Textbooks and References/Materials/catalog.ts
+++ src/content/docs/Courses, Textbooks and References/Materials/catalog.ts
@@ -1,27 +1,41 @@
 import * as base from '../base.ts'
 
-type legal_value_types_for_ID = PropertyKey | bigint
-type ID_t = legal_value_types_for_ID | object
+type legal_primitives_for_ID = PropertyKey | bigint
+type legal_objects_for_ID = {
+  unordered_author?: string | string[]
+  ordered_author?: string | string[]
+  title?: string,
+  edition?: string | number
+  date?: string
+  vol?: string | number
+}
+const legal_object_keys_for_ID = [
+  'unordered_author',
+  'ordered_author',
+  'title',
+  'edition',
+  'date',
+  'vol',
+]
+type ID_t = legal_primitives_for_ID | [ legal_objects_for_ID, ...legal_primitives_for_ID[] ] | legal_objects_for_ID
 
 const m = new Map<ID_t, base.Course_Material>
 
-export function add(IDs: ID_t[], material: base.Course_Material) {
-  for (const ID of IDs) {
-    switch (typeof ID) {
-      case 'string':
-      case 'number':
-      case 'bigint':
-      case 'symbol':
-        m.set(ID, material)
-        break
-      case 'object':
-        if (Array.isArray(IDs)) { // n-tuple only
-          m.set(JSON.stringify(ID), material)
-        } else {
-          throw new Error(`ID cannot be string-indexed object`)
-        }
-    }
+export function canonical_ID(ID: ID_t): ID_t {
+  switch (typeof ID) {
+    case 'string':
+    case 'number':
+    case 'bigint':
+    case 'symbol':
+      return ID
+    case 'object':
+      if (Array.isArray(ID)) { return JSON.stringify(ID) } // ordered n-tuple
+      else { return JSON.stringify(ID, legal_object_keys_for_ID) }
   }
+}
+
+export function add(IDs: ID_t[], material: base.Course_Material) {
+  for (const ID of IDs) { m.set(canonical_ID(ID), material) }
 }
 
 export function get(ID: ID_t): base.Course_Material {
