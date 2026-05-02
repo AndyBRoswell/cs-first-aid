f575ee22464f0b6bf3de90c93ee114fa9b8bc3c8
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2026-05-03 00:43:05 +0200

    Use @/types/data.ts:ID_t as ID when calling @/content/docs/base.ts:cite

--- src/types/data.ts
+++ src/types/data.ts
@@ -1,5 +1,25 @@
 import * as CSL_Data from './CSL_data.ts'
 
+type ID_primitive = PropertyKey | bigint
+type ID_object = {
+  unordered_author?: string | string[]
+  ordered_author?: string | string[]
+  title?: string,
+  edition?: string | number
+  date?: string | number
+  volume?: string | number
+}
+export const legal_object_keys_for_ID = [
+  'unordered_author',
+  'ordered_author',
+  'title',
+  'edition',
+  'date',
+  'volume',
+]
+export type ID_t = ID_primitive | [ ID_primitive, ...ID_primitive[] ] | ID_object
+export type Item = { id: ID_t[], material: CSL_Data.Item, }
+
 export type Link = string | { display_text?: string, link: string, }
 
 export type Course = {
