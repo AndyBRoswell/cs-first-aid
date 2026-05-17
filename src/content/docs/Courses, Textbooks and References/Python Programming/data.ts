b233a2b5bcf714cfc4052659d6f09965695a18f6
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2026-05-18 00:05:56 +0200

    嵩天 等 Python.ts

--- src/content/docs/Courses, Textbooks and References/Python Programming/data.ts
+++ src/content/docs/Courses, Textbooks and References/Python Programming/data.ts
@@ -3,6 +3,7 @@ import * as data_type from '@/types/data.ts'
 import * as catalog from '@/data/materials/catalog.ts'
 import '@/data/materials/PSF, The Python Tutorial.ts'
 import '@/data/materials/E. Matthes, Python Crash Course.ts'
+import '@/data/materials/嵩天 等 Python.ts'
 
 export const info = {
   canonical_name: 'Python 程序设计',
@@ -14,6 +15,7 @@ export const info = {
     ],
     other: [
       catalog.get({ unordered_author: 'E. Matthes', title: 'Python Crash Course', }),
+      catalog.get({ unordered_author: [ '嵩天', '黄天羽', '杨雅婷' ], title: 'Python语言程序设计基础', }),
     ],
   }
 } satisfies data_type.Course
