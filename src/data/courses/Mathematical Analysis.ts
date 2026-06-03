352dc8bf0a6cf5fc7d3f29df5acab8d545d3ce41
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2026-06-03 02:44:12 +0200

    restructured project dir: put course data into @/data/courses

--- src/data/courses/Mathematical Analysis.ts
+++ src/data/courses/Mathematical Analysis.ts
@@ -1,7 +1,7 @@
-import * as docs_base from '../../base.ts'
+import * as docs_base from '@/content/docs/base.ts'
 import * as data_type from '@/types/data.ts'
 import * as catalog from '@/data/materials/catalog.ts'
-import '@/content/docs/Courses, Textbooks and References/import course materials.ts'
+import '@/data/courses/import course materials.ts'
 
 export const info = {
   canonical_name: '数学分析',
