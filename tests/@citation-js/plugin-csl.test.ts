828eb2bfc1b968cb9584d0c8c2658ef84a37af49
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2026-06-04 22:45:25 +0200

    unit test: @citation-js/plugin-csl.output.citation

---
+++ tests/@citation-js/plugin-csl.test.ts
@@ -0,0 +1,34 @@
+import { test, expect } from '@playwright/test';
+
+// @ts-ignore [citation-js doesn't have ts support]
+import citation_js from "@citation-js/core";
+import '@citation-js/plugin-csl'
+import { readFileSync } from 'node:fs'
+import { resolve } from 'node:path'
+
+const CSL_config = citation_js.plugins.config.get('@csl')
+const default_bib_style_name = 'IEEE Custom'
+const default_bib_style_file = resolve(import.meta.dirname, '../../src/content/docs/ieee.csl')
+const default_bib_style = readFileSync(default_bib_style_file, 'utf8')
+CSL_config.templates.add(default_bib_style_name, default_bib_style)
+const prettified_default_bib_style: object = {
+  format: 'text',
+  template: default_bib_style_name,
+  hyperlinks: true,
+}
+
+test('@citation-js/plugin-csl.output.citation', { tag: [ '@citation-js/plugin-csl.output.citation', ], }, async () => {
+  let cite = new citation_js.Cite([
+    { id: 'a', title: 'Item A', issued: { 'date-parts': [ [ 2016 ] ] } },
+    { id: 'b', title: 'Item B', issued: { 'date-parts': [ [ 2017 ] ] } },
+    { id: 'c', title: 'Item C', issued: { 'date-parts': [ [ 2018 ] ] } }
+  ])
+  expect(cite.format('citation')).toEqual('(Item A, 2016; Item B, 2017; Item C, 2018)')
+  expect(cite.format('citation', { entry: [ 'a', 'b' ] })).toEqual('(Item A, 2016; Item B, 2017)')
+  expect(cite.format('citation', { entry: 'a' })).toEqual('(Item A, 2016)')
+  expect(cite.format('citation', { entry: [ { id: 'a', label: 'page', locator: 123 } ] })).toEqual('(Item A, 2016, p. 123)')
+  expect(cite.format('citation', {
+    entry: [ { id: 'c', label: 'page', locator: 1234, } ],
+    ...prettified_default_bib_style
+  })).toEqual('[1, p. 1234]')
+})
