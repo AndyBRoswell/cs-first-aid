3648d34951e59437927a3e5db468f12bf943dd1b
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2026-07-09 01:06:40 +0200

    import package names instead of functions inside

--- tests/@citation-js/plugin-csl.test.ts
+++ tests/@citation-js/plugin-csl.test.ts
@@ -3,14 +3,14 @@ import { test, expect } from '@playwright/test';
 // @ts-ignore [citation-js doesn't have ts support]
 import citation_js from "@citation-js/core";
 import '@citation-js/plugin-csl'
-import { readFileSync } from 'node:fs'
-import { resolve } from 'node:path'
-import { parse } from 'node-html-parser'
+import node_fs from 'node:fs'
+import node_path from 'node:path'
+import node_html_parser from 'node-html-parser'
 
 const CSL_config = citation_js.plugins.config.get('@csl')
 const default_bib_style_name = 'IEEE Custom'
-const default_bib_style_file = resolve(import.meta.dirname, '../../src/content/docs/IEEE.custom.csl')
-const default_bib_style = readFileSync(default_bib_style_file, 'utf8')
+const default_bib_style_file = node_path.resolve(import.meta.dirname, '../../src/content/docs/IEEE.custom.csl')
+const default_bib_style = node_fs.readFileSync(default_bib_style_file, 'utf8')
 CSL_config.templates.add(default_bib_style_name, default_bib_style)
 const prettified_default_bib_style: object = {
   format: 'html',
@@ -26,11 +26,11 @@ const cite = new citation_js.Cite([
 ])
 
 test('@citation-js/plugin-csl.output.citation', { tag: [ '@citation-js/plugin-csl.output.citation', ], }, () => {
-  expect(parse(cite.format('citation')).textContent).toEqual('(Item A, 2016; Item B, 2017; Item C, 2018)')
-  expect(parse(cite.format('citation', { entry: [ 'a', 'b' ] })).textContent).toEqual('(Item A, 2016; Item B, 2017)')
-  expect(parse(cite.format('citation', { entry: 'a' })).textContent).toEqual('(Item A, 2016)')
-  expect(parse(cite.format('citation', { entry: [ { id: 'a', label: 'page', locator: 123 } ] })).textContent).toEqual('(Item A, 2016, p. 123)')
-  expect(parse(cite.format('citation', {
+  expect(node_html_parser.parse(cite.format('citation')).textContent).toEqual('(Item A, 2016; Item B, 2017; Item C, 2018)')
+  expect(node_html_parser.parse(cite.format('citation', { entry: [ 'a', 'b' ] })).textContent).toEqual('(Item A, 2016; Item B, 2017)')
+  expect(node_html_parser.parse(cite.format('citation', { entry: 'a' })).textContent).toEqual('(Item A, 2016)')
+  expect(node_html_parser.parse(cite.format('citation', { entry: [ { id: 'a', label: 'page', locator: 123 } ] })).textContent).toEqual('(Item A, 2016, p. 123)')
+  expect(node_html_parser.parse(cite.format('citation', {
     entry: [ { id: 'c', label: 'page', locator: 1234, } ],
     ...prettified_default_bib_style
   })).textContent).toEqual('[1, p. 1234]')
