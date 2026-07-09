63bd6684e5a8bd218f9f706a7f12e99bf0629682
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2026-07-09 03:50:44 +0200

    test 'course list'

--- playwright.config.ts
+++ playwright.config.ts
@@ -73,9 +73,9 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://localhost:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: 'npm run preview',
+    url: 'http://localhost:4321',
+    reuseExistingServer: !process.env.CI,
+  },
 });
