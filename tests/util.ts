f065c60182f01189f9016a404cc0b60efb331a6b
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2026-07-10 23:33:40 +0200

    extract common stuff

--- tests/util.ts
+++ tests/util.ts
@@ -1,6 +1,7 @@
 import pino from 'pino'
 
 export const pino_arg: pino.LoggerOptions = {
+  level: process.env.log_level || 'info',
   transport: {
     target: 'pino-pretty',
     options: {
@@ -14,3 +15,8 @@ export const pino_arg: pino.LoggerOptions = {
 }
 
 export const test_server = process.env.test_server || 'http://localhost:4321'
+
+export function on_pageerror(errors: Error[], error: Error) {
+  if (error.message.includes('error loading dynamically imported module')) return
+  errors.push(error)
+}
