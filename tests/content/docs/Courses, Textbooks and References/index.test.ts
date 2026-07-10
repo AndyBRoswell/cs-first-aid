9bc10ee83f83caede47a4be4358fe667840ef56c
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2026-07-10 23:33:40 +0200

    extract common stuff

--- tests/content/docs/Courses, Textbooks and References/index.test.ts
+++ tests/content/docs/Courses, Textbooks and References/index.test.ts
@@ -3,26 +3,20 @@ import pino from 'pino'
 import * as util from '../../../util.ts'
 
 const pino_arg = util.pino_arg
-pino_arg.level = process.env.log_level || 'info'
 
 const logger = pino(pino_arg)
-
-function on_pageerror(errors: Error[], error: Error) {
-  if (error.message.includes('error loading dynamically imported module')) return
-  errors.push(error)
-}
 
 test('course list', { tag: [ '@course list' ] }, async ({ page, context }) => {
   const prefix: string = `${util.test_server}/courses-textbooks-and-references`
   const errors: Error[] = []
-  page.on('pageerror', (error: Error) => on_pageerror(errors, error))
+  page.on('pageerror', (error: Error) => util.on_pageerror(errors, error))
   const response = await page.goto(`${prefix}/#%E8%AF%BE%E7%A8%8B%E5%88%97%E8%A1%A8`) // courses-textbooks-and-references/#课程列表
   expect(response!.status()).toBe(200)
   expect(errors).toHaveLength(0)
   const course_list = page.getByTestId('course list')
   const links = await course_list.locator('[href]').all()
   const tmp_page = await context.newPage()
-  tmp_page.on('pageerror', (error: Error) => on_pageerror(errors, error))
+  tmp_page.on('pageerror', (error: Error) => util.on_pageerror(errors, error))
   for (const link of links) {
     const href = await link.getAttribute('href')
     const target_link = `${prefix}/${href}`
