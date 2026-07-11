011a83e32a7fdef4d7dcd2e0d2c17b27376c4989
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2026-07-11 19:37:44 +0200

    del files from astro tutorial

--- src/scripts/menu.js
+++
@@ -1,5 +0,0 @@
-const menu = document.querySelector('.menu');
-menu?.addEventListener('click', () => {
-  const isExpanded = menu.getAttribute('aria-expanded') === 'true';
-  menu.setAttribute('aria-expanded', `${!isExpanded}`);
-});
