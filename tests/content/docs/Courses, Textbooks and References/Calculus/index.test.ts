4f6b826002477e20c4848f1e269e705f2b49220f
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2026-07-13 06:03:55 +0200

    adding test for course page

---
+++ tests/content/docs/Courses, Textbooks and References/Calculus/index.test.ts
@@ -0,0 +1,20 @@
+import { expect } from '@playwright/test';
+import pino from 'pino'
+import * as util from '../../../../util.ts'
+import * as data from "@/types/data.ts";
+
+const logger = pino(util.pino_arg)
+
+util.test('Calculus', { tag: [ '@Courses, Textbooks and References', '@Calculus' ] }, async ({ page, errors }) => {
+  const response = await page.goto(`${util.test_server}/courses-textbooks-and-references/calculus`)
+  page.on('pageerror', (error: Error) => util.on_pageerror(errors, error))
+  expect(response!.status()).toBe(200)
+  expect(errors).toHaveLength(0)
+  const References_locators = await page.locator('.References').all()
+  for (const locator of References_locators) {
+    const csl_entries = await locator.locator('[data-csl-entry-id]').all()
+    const material_segment = JSON.parse((await locator.getAttribute('data-material_segment'))!) as Array<data.Material>
+    expect(material_segment.length).toEqual(csl_entries.length)
+  }
+  await page.getByRole('heading', { name: '教科书', level: 3 })
+})
