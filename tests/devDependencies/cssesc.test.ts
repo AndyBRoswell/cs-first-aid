dddc14d518e39130446c2f1f5870f7cbb370cb3c
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2026-07-14 22:20:43 +0200

    refined test `cssesc`

--- tests/devDependencies/cssesc.test.ts
+++ tests/devDependencies/cssesc.test.ts
@@ -4,7 +4,7 @@ import cssesc from 'cssesc'
 import * as util from '@tests/util.ts'
 
 test('cssesc', async () => {
-  const scope_name: data.Scope_Name = [ 'textbook', 'primary', 'zh-CN' ]
+  const scope_name: data.Scope_Name = [ 'references', 'textbook', 'primary', 'zh-CN' ]
   const escaped_scope_name: data.Serialized_Scope_Name = cssesc(JSON.stringify(scope_name), util.cssesc_options)
-  expect(escaped_scope_name).toEqual(`[\\"textbook\\",\\"primary\\",\\"zh-CN\\"]`)
+  expect(escaped_scope_name).toEqual(`[\\"references\\",\\"textbook\\",\\"primary\\",\\"zh-CN\\"]`)
 })
