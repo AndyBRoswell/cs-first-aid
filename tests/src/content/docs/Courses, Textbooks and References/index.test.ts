1e2195f7e73cbbe209f2e2136c6a51bf4bf2c819
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2026-07-14 18:01:40 +0200

    restructured test code

--- tests/src/content/docs/Courses, Textbooks and References/index.test.ts
+++ tests/src/content/docs/Courses, Textbooks and References/index.test.ts
@@ -1,15 +1,13 @@
 import { expect } from '@playwright/test';
 import pino from 'pino'
 import * as util from '@tests/util.ts'
+import * as src_util from '@tests/src/util.ts'
 
 const logger = pino(util.pino_arg)
 
 util.test('course list', { tag: [ '@Courses, Textbooks and References', '@course list' ] }, async ({ page, context, errors }) => {
   const prefix: string = `${util.test_server}/courses-textbooks-and-references`
-  page.on('pageerror', (error: Error) => util.on_pageerror(errors, error))
-  const response = await page.goto(`${prefix}/#%E8%AF%BE%E7%A8%8B%E5%88%97%E8%A1%A8`) // courses-textbooks-and-references/#课程列表
-  expect(response!.status()).toBe(200)
-  expect(errors).toHaveLength(0)
+  await src_util.goto(page, errors, `${prefix}/#%E8%AF%BE%E7%A8%8B%E5%88%97%E8%A1%A8`) // courses-textbooks-and-references/#课程列表
   const course_list = page.getByTestId('course list')
   const links = await course_list.locator('[href]').all()
   const tmp_page = await context.newPage()
