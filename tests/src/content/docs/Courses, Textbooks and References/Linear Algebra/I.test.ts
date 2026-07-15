d90943b179704adba4ad1f28ed0477a79410728c
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2026-07-15 19:46:23 +0200

    test `Linear Algebra I`

---
+++ tests/src/content/docs/Courses, Textbooks and References/Linear Algebra/I.test.ts
@@ -0,0 +1,19 @@
+import { expect, type Locator } from '@playwright/test'
+import * as util from '@tests/util.ts'
+import * as src_util from '@tests/src/util.ts'
+import * as course_util from '@tests/src/content/docs/Courses, Textbooks and References/util.ts'
+
+src_util.test('Linear Algebra I', { tag: [ '@Courses, Textbooks and References', '@Linear Algebra I', '@Linear Algebra', '@LinAlg', ] }, async ({ page }) => {
+  await page.goto(`${util.test_server}/courses-textbooks-and-references/linear-algebra/i`)
+
+  const main = page.getByRole('main')
+
+  await course_util.check_references(main)
+
+  await expect(main.locator('#_top')).toHaveText(/线性代数\s*I/)
+
+  let section: Locator, heading: Locator, References: Locator
+
+  section = main.locator('section', { has: page.getByRole('heading', { level: 1, name: '学习材料' }) })
+  await expect(section).toHaveCount(1)
+})
