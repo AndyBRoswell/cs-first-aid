50d9c68b35c187422a8031ce1206ca9b8d66c4ed
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2026-07-16 03:43:46 +0200

    update test `Calculus`

--- tests/src/content/docs/Courses, Textbooks and References/Calculus/index.test.ts
+++ tests/src/content/docs/Courses, Textbooks and References/Calculus/index.test.ts
@@ -17,11 +17,15 @@ src_util.test('Calculus', { tag: [ '@Courses, Textbooks and References', '@Calcu
   heading = main.getByRole('heading', { level: 1, name: '学习材料' })
   await expect(heading).toHaveCount(1)
 
-  References = await course_util.locate_references(main, [ 'text', 'selected' ])
-  section = References.locator('..')
-  heading = section.getByRole('heading', { level: 2, name: '教科书' })
-  await expect(heading).toHaveCount(1)
-  section = section.locator('section', { has: page.getByRole('heading', { level: 3, name: '说明' }) }) // NOTE: the `Locator` passed to `has` must stem from `page`
+  References = course_util.locate_references(main, [ 'text', 'selected' ])
+  await src_util.everyone_occurs(References, [
+    /R. A. Adams/,
+    /龚昇/,
+    /Calculus: A Complete Course/,
+    /简明微积分/,
+  ])
+  section = src_util.locate_parent(References, 'section')
+  section = section.locator('section', { has: page.getByRole('heading', { level: 3, name: '说明' }) })
   await expect(section).toHaveCount(1)
   await src_util.everyone_occurs(section, [
     /《Calculus: A Complete Course》/,
@@ -29,19 +33,20 @@ src_util.test('Calculus', { tag: [ '@Courses, Textbooks and References', '@Calcu
     /《简明微积分》/,
   ])
 
-  References = await course_util.locate_references(main, [ 'text', 'excluded' ])
-  section = References.locator('..')
-  heading = section.getByRole('heading', { level: 3, name: '未被选择的书目' })
-  await expect(heading).toHaveCount(1)
+  References = course_util.locate_references(main, [ 'text', 'excluded' ])
+  section = src_util.locate_parent(References, 'section')
+  section = section.locator('section', { has: page.getByRole('heading', { level: 4, name: '说明' }) })
+  await expect(section).toHaveCount(1)
   await src_util.everyone_occurs(section, [
-    /不低于同济大学数学系主编《高等数学》/,
-    /荣誉.*ECE.+EECS/,
+    /基准/,
+    /同济/,
+    /ECE/,
+    /EECS/,
+    /荣誉/,
   ])
 
-  References = await course_util.locate_references(main, [ 'other', 'text', ])
-  section = References.locator('..')
-  heading = section.getByRole('heading', { level: 2, name: '其它参考' })
-  await expect(heading).toHaveCount(1)
+  References = course_util.locate_references(main, [ 'other', 'text', ])
+  section = src_util.locate_parent(References, 'section')
   section = section.locator('section', { has: page.getByRole('heading', { level: 3, name: '说明' }) })
   await expect(section).toHaveCount(1)
   await src_util.everyone_occurs(section, [
@@ -50,13 +55,13 @@ src_util.test('Calculus', { tag: [ '@Courses, Textbooks and References', '@Calcu
     /Apostol/,
   ])
 
-  heading = main.getByRole('heading', { level: 1, name: '院校开课情况选讲' })
+  heading = main.getByRole('heading', { level: 1, name: /开课/ })
   await expect(heading).toHaveCount(1)
-  section = src_util.locate_parent(heading, 'section', 1)
+  section = src_util.locate_parent(heading, 'section')
   await src_util.everyone_occurs(section, [
     /学期/,
-    /单变量微积分/,
-    /多变量微积分/,
+    /单变量/,
+    /多变量/,
     /(矢量|向量)微积分/,
   ])
 })
