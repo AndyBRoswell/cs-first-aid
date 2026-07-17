0e977b2b689c4e060052008c8f695700ad64c77a
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2026-07-17 22:47:26 +0200

    migrating unit tests to vitest to avoid unnecessary launches of browsers

--- tests/vitest/devDependencies/cssesc.test.ts
+++ tests/vitest/devDependencies/cssesc.test.ts
@@ -1,9 +1,10 @@
-import { test, expect, } from '@playwright/test'
+import { test, expect, } from 'vitest'
+
 import * as data from "@/types/data.ts"
 import cssesc from 'cssesc'
 import * as util from '@tests/util.ts'
 
-test('cssesc', async () => {
+test('cssesc', { tags: [ 'cssesc', ] }, async () => {
   const scope_name: data.Scope_Name = [ 'references', 'textbook', 'primary', 'zh-CN' ]
   const escaped_scope_name: data.Serialized_Scope_Name = cssesc(JSON.stringify(scope_name), util.cssesc_options)
   expect(escaped_scope_name).toEqual(`[\\"references\\",\\"textbook\\",\\"primary\\",\\"zh-CN\\"]`)
--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,17 @@ import path from 'node:path';
 export default defineConfig({
   test: {
     dir: 'tests/vitest',
+    tags: [
+      {
+        name: '@citation-js/plugin-csl.output.citation',
+      },
+      {
+        name: 'fast-cartesian',
+      },
+      {
+        name: 'cssesc',
+      },
+    ],
   },
   resolve: {
     alias: {
