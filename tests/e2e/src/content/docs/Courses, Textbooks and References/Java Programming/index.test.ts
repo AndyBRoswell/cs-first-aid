b043b17199d55f8846aa87eed977cf8a53b521e1
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2026-07-18 20:21:33 +0200

    test `Java Programming`

---
+++ tests/e2e/src/content/docs/Courses, Textbooks and References/Java Programming/index.test.ts
@@ -0,0 +1,36 @@
+import { expect, type Locator } from '@playwright/test'
+import * as util from '@tests/util.ts'
+import * as src_util from '@tests/e2e/src/util.ts'
+import * as docs_util from '@tests/e2e/src/content/docs/util.ts'
+import * as course_util from '@tests/e2e/src/content/docs/Courses, Textbooks and References/util.ts'
+
+src_util.test('Java Programming', { tag: [ '@Courses, Textbooks and References', '@Java Programming' ] }, async ({ page }) => {
+  await page.goto(`${util.test_server}/courses-textbooks-and-references/java-programming`)
+
+  const main = page.getByRole('main')
+
+  await course_util.check_references(main)
+
+  await docs_util.check_title(main, /Java\s*程序设计/)
+
+  let section: Locator, heading: Locator, References: Locator
+
+  heading = main.getByRole('heading', { level: 1, name: '学习材料' })
+  await expect(heading).toHaveCount(1)
+
+  await src_util.test.step('教科书', async () => {
+    References = course_util.locate_references(main, [ 'text' ])
+    await src_util.everyone_occurs(References, [
+      /Oracle/,
+      /Learn Java/,
+    ])
+  })
+
+  await src_util.test.step('参考资料', async () => {
+    References = course_util.locate_references(main, [ 'reference' ])
+    await src_util.everyone_occurs(References, [
+      /K. Sierra/,
+      /Head First Java/,
+    ])
+  })
+})
