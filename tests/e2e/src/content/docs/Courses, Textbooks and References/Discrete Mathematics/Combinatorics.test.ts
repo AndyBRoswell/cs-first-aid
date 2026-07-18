dfc774456c9186bd03861803709396e3b32153cc
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2026-07-18 18:35:53 +0200

    test 'Combinatorics'

---
+++ tests/e2e/src/content/docs/Courses, Textbooks and References/Discrete Mathematics/Combinatorics.test.ts
@@ -0,0 +1,71 @@
+import { expect, type Locator } from '@playwright/test'
+import * as util from '@tests/util.ts'
+import * as src_util from '@tests/e2e/src/util.ts'
+import * as docs_util from '@tests/e2e/src/content/docs/util.ts'
+import * as course_util from '@tests/e2e/src/content/docs/Courses, Textbooks and References/util.ts'
+
+src_util.test('Combinatorics', { tag: [ '@Courses, Textbooks and References', '@Discrete Mathematics', '@Combinatorics' ] }, async ({ page }) => {
+  await page.goto(`${util.test_server}/courses-textbooks-and-references/discrete-mathematics/combinatorics`)
+
+  const main = page.getByRole('main')
+
+  await course_util.check_references(main)
+
+  await docs_util.check_title(main, /组合数?学/)
+
+  let section: Locator, heading: Locator, References: Locator
+
+  heading = main.getByRole('heading', { level: 1, name: '学习材料' })
+  await expect(heading).toHaveCount(1)
+
+  await src_util.test.step('学习材料/教科书/中文', async () => {
+    References = course_util.locate_references(main, [ 'text', 'zh' ])
+    await src_util.everyone_occurs(References, [
+      /王捍贫/,
+      /张立昂/,
+      /离散数学教程/,
+    ])
+  })
+
+  await src_util.test.step('学习材料/教科书/英文', async () => {
+    References = course_util.locate_references(main, [ 'text', 'en' ])
+    await src_util.everyone_occurs(References, [
+      /J. M. Harris/,
+      /Combinatorics and Graph Theory/,
+    ])
+  })
+
+  await src_util.test.step('学习材料/公开课/中文', async () => {
+    References = course_util.locate_references(main, [ 'open_course', 'zh' ])
+    await src_util.everyone_occurs(References, [
+      /离散数学/,
+      /北京大学/,
+    ])
+  })
+
+  await src_util.test.step('学习材料/指导书/中文', async () => {
+    References = course_util.locate_references(main, [ 'guide', 'zh' ])
+    await src_util.everyone_occurs(References, [
+      /张立昂/,
+      /刘田/,
+      /离散数学习题解析/,
+      /离散数学学习指导与习题解析/,
+    ])
+  })
+
+  await src_util.test.step('其它参考/教科书/英文', async () => {
+    References = course_util.locate_references(main, [ 'reference', 'text', 'en' ])
+    await src_util.everyone_occurs(References, [
+      /K. H. Rosen/,
+      /Discrete Mathematics and Its Applications/,
+    ])
+  })
+
+  await src_util.test.step('其它参考/指导书/英文', async () => {
+    References = course_util.locate_references(main, [ 'reference', 'guide', 'en' ])
+    await src_util.everyone_occurs(References, [
+      /K. H. Rosen/,
+      /Student['’]s Solutions Guide for Discrete Mathematics and Its Applications/,
+    ])
+  })
+})
