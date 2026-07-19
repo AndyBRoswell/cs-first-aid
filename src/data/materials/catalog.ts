54e8f8f9dd81d750791aba3c99ca680f5c40043f
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2026-07-19 16:59:43 +0200

    in progress: `enumerate_canonical_IDs`

--- src/data/materials/catalog.ts
+++ src/data/materials/catalog.ts
@@ -1,6 +1,7 @@
 import type { ID_t, Entry, Material, ID_primitive, ID_object } from "@/types/data.ts";
 import { legal_keys_of_ID_object } from "@/types/data.ts";
 import * as CSL_Data from '@/types/CSL_data.ts'
+import Cartesian_product from "fast-cartesian";
 
 const m = new Map<ID_t, Material>
 
@@ -27,21 +28,46 @@ export function canonical_ID(ID: ID_t): ID_primitive {
 }
 
 export type params_of_canonical_ID_enumeration = {
-  group: (keyof ID_object)[][]
+  choice: (keyof ID_object)[][]
   optional?: boolean
-  values?: { [key in keyof ID_object]?: { value: ID_object[key][], override?: boolean } }
+  values?: { [key in keyof ID_object]?: { value: ID_object[key][], override?: boolean } } // todo: handle extra values/values used to override
 }[]
 
 export function enumerate_canonical_IDs(
   groups: params_of_canonical_ID_enumeration = [
-    { group: [ [ 'unordered_author' ], [ 'ordered_author' ] ] },
-    { group: [ [ 'title', 'subtitle' ] ] },
-    { group: [ [ 'edition' ], [ 'date' ] ], optional: true, },
-    { group: [ [ 'volume', 'part' ] ] },
+    { choice: [ [ 'unordered_author' ], [ 'ordered_author' ] ] },
+    { choice: [ [ 'title', 'subtitle' ] ] },
+    { choice: [ [ 'edition' ], [ 'date' ] ], optional: true, },
+    { choice: [ [ 'volume', 'part' ] ] },
   ],
   material?: Material,
 ): ID_t[] {
-// todo
+  const ret: ID_t[] = []
+  const enum_task_components = new Array<boolean[]>(groups.length).fill([])
+  for (const [ index, group ] of groups.entries()) {
+    enum_task_components[index]!.push(true)
+    if (group.optional === true) { enum_task_components[index]!.push(false) }
+  }
+  const enum_tasks = Cartesian_product(enum_task_components) // ex. TTTT and TTFT for the default of `groups`
+  for (const task of enum_tasks) {
+    const target_choices = []
+    for (const [ index, enabled ] of task.entries()) { if (enabled) { target_choices.push(groups[index]!.choice) } }
+    let ID_component_combinations = Cartesian_product(target_choices)
+    for (const combination of ID_component_combinations) {
+      const ID: ID_t = {}
+      for (const component in combination) {
+        switch (component) {
+          case 'unordered_author': case 'ordered_author':
+            break // todo: use citation-js to print bib for a dummy item
+          case 'date':
+            break // todo: use citation-js to print bib for a dummy item
+          default:
+            break
+        }
+      }
+    }
+  }
+  return ret
 }
 
 export function add_items(p: Entry[]) {
