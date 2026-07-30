310b23e3f6d86a20e715ec4336b277f03c3eecc6
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2026-07-30 21:52:49 +0200

    add logger

--- src/data/courses/import course materials.ts
+++ src/data/courses/import course materials.ts
@@ -1,7 +1,13 @@
 // import 'virtual:import-course-materials'
 
+import pino from 'pino'
 import * as catalog from '@/data/materials/catalog.ts'
+import * as util from "@/util.ts";
 
+const logger = pino(util.pino_arg)
+
+logger.info('Importing course materials...')
 const course_materials = import.meta.glob('../materials/*.ts', { eager: true });
+logger.info('Course materials loaded.')
 
 await catalog.dump_locally()
