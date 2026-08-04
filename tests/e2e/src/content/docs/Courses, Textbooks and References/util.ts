fa958ac2261b27a05214b22036a90b785f0a1b25
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2026-08-04 03:11:21 +0200

    test whether custom data can be shown

--- tests/e2e/src/content/docs/Courses, Textbooks and References/util.ts
+++ tests/e2e/src/content/docs/Courses, Textbooks and References/util.ts
@@ -1,5 +1,6 @@
 import { expect, type Locator } from '@playwright/test'
 import * as data from "@/types/data.ts";
+import * as catalog from '@/data/materials/catalog.ts'
 // @ts-ignore [css.escape doesn't have ts support]
 import cssesc from "cssesc";
 import * as util from "@/util.ts";
@@ -12,9 +13,22 @@ export function locate_references(main: Locator, scope_name: data.Scope_Name) {
 export async function check_references(main: Locator) {
   const References_locators = await main.locator('.References').all()
   for (const locator of References_locators) {
+    // basic
     const CSL_entries = await locator.locator('.CSL_Entry').all()
     await expect(locator).toHaveAttribute('data-scope_name')
     const material_segment = JSON.parse((await locator.getAttribute('data-material_segment'))!) as Array<data.Material>
     expect(material_segment.length).toEqual(CSL_entries.length)
+    // custom data
+    for (const [ index, material ] of material_segment.entries()) {
+      if ('custom' in material) {
+        if ('lecturer' in material.custom) {
+          const custom_div: Locator = CSL_entries[index]!.locator('.custom')
+          const lecturer_p: Locator = custom_div.locator('.lecturer')
+          const rendered_lecturer: string = catalog.get_rendered_names(material.custom.lecturer)
+          const expected = `Lecturer: ` + rendered_lecturer
+          await expect(lecturer_p).toHaveText(expected)
+        }
+      }
+    }
   }
 }
