8d64bc8e5cc37f5de9ea8524ad2ba3728c883ef7
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2026-08-07 20:51:50 +0200

    add tailwind

--- astro.config.ts
+++ astro.config.ts
@@ -72,6 +72,7 @@ export default defineConfig({
         { icon: 'github', label: 'GitHub', href: 'https://github.com/AndyBRoswell/cs-first-aid' },
       ],
       customCss: [
+        './src/styles/global.css',
         './src/styles/docs.css',
       ],
       locales: {
