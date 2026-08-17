212ffea0e728275dcc71ffb820673a1fef864af0
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2026-08-18 00:43:51 +0200

    alpha: 了解行业

--- apps/main/src/components/sidebar/提升效率的工具与思想.ts
+++ apps/main/src/components/sidebar/提升效率的工具与思想.ts
@@ -144,7 +144,7 @@ export const Tools_and_Ideas_to_Improve_Efficiency = {
       },
       slug: '',
       attrs: {
-        'data-release-stage': release_stages.to_HTML_attr({ 'zh-CN': 'blank', en: 'blank', })
+        'data-release-stage': release_stages.to_HTML_attr({ 'zh-CN': '2026.1.0-alpha', en: 'blank', })
       },
     },
     {
