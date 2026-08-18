a6d1761af93c29fb8f3825cbee120fab3117e7bf
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2026-08-18 02:51:03 +0200

    ToC

--- apps/main/src/config/starlight.ts
+++ apps/main/src/config/starlight.ts
@@ -4,7 +4,7 @@ import type { StarlightUserConfig } from '@astrojs/starlight/types'
 import { Preface } from '@/components/sidebar/前言.ts'
 import { Tools_and_Ideas_to_Improve_Efficiency } from '@/components/sidebar/提升效率的工具与思想.ts'
 import { Courses_Textbooks_and_References } from '@/components/sidebar/课程、教科书与参考资料.ts'
-import { Things_Needed_to_Be_Started_or_Done_as_Soon_as_Possible } from '@/components/sidebar/需要尽早开始或完成的事项.ts'
+import { Critical_Goals_and_Time_Windows } from '@/components/sidebar/关键目标与时间窗口.ts'
 import { Future_Trends } from '@/components/sidebar/未来趋势.ts'
 import { FAQ } from '@/components/sidebar/常见问题.ts'
 import { Recommended_Reading_Materials } from '@/components/sidebar/推荐读物.ts'
@@ -27,7 +27,7 @@ export const sidebar = [
   Preface,
   Tools_and_Ideas_to_Improve_Efficiency,
   Courses_Textbooks_and_References,
-  Things_Needed_to_Be_Started_or_Done_as_Soon_as_Possible,
+  Critical_Goals_and_Time_Windows,
   Future_Trends,
   FAQ,
   Recommended_Reading_Materials,
