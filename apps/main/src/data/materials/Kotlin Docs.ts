64217de0d591e32526dff01b480ddd0e1215a080
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2026-08-19 17:07:48 +0200

    `Kotlin Docs.ts`

---
+++ apps/main/src/data/materials/Kotlin Docs.ts
@@ -0,0 +1,28 @@
+import * as catalog from '@cs-first-aid/bibkit/catalog'
+import * as types_data from '@cs-first-aid/bibkit/types/data'
+import * as CSL from '@cs-first-aid/bibkit/CSL'
+
+const docs = [
+  {
+    id: [ 'Kotlin Docs', 'Kotlin Documentation', 'Kotlin Reference' ],
+    material: {
+      type: 'webpage',
+      title: 'Kotlin Documentation',
+      author: [ { literal: 'JetBrains' } ],
+      language: 'en-US',
+      URL: 'https://kotlinlang.org/docs/home.html',
+      issued: { 'date-parts': [ [ 2026, 8, 13 ] ] },
+      accessed: { 'date-parts': [ [ 2026, 8, 14 ] ] },
+      custom: {
+        GitHub: {
+          type: 'webpage',
+          URL: 'https://github.com/JetBrains/kotlin/tree/master/docs',
+          issued: { 'date-parts': [ [ 2026, 8, 13 ] ] },
+          accessed: { 'date-parts': [ [ 2026, 8, 14 ] ] },
+        } satisfies types_data.Material,
+      } satisfies CSL.Custom,
+    },
+  },
+] satisfies types_data.Entry[]
+
+catalog.add_items(docs)
