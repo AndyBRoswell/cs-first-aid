3ec88f57cc7e95f7021d4fe003c8e4ba3cca1afc
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2026-08-28 13:17:11 +0200

    add test timeout

--- apps/main/playwright.config.ts
+++ apps/main/playwright.config.ts
@@ -14,7 +14,7 @@ import node_os from 'node:os'
  */
 export default defineConfig({
   testDir: './tests/e2e',
-  timeout: 0, // See https://playwright.dev/docs/test-timeouts
+  timeout: 60_000, // See https://playwright.dev/docs/test-timeouts
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -32,6 +32,9 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+  },
+  expect: {
+    timeout: 5_000,
   },
 
   /* Configure projects for major browsers */
