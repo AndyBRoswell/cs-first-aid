ed567eeef1ac43d724120822864f41e82e3ee9ec
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2026-09-09 19:14:59 +0200

    reorganizing page structure: moving derivations to separate pages

--- apps/main/src/content/docs/Derivations/Displays, Light and Vision.ts
+++ apps/main/src/content/docs/Derivations/Displays, Light and Vision.ts
@@ -4,5 +4,7 @@ import '@/data/materials/import materials.ts'
 
 export const references = [
   ...catalog.filter(m => m.type === 'paper-conference' && util.ieq(m.title!, 'Optimal Rendering for Colour Matrix Displays'), { count: 1 }),
+  catalog.get('ISO 8596:2017'),
+  catalog.get('GB/T 11533-2011'),
 ]
 
