b42317bd936670b765b8850407d04e2729239d6c
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2026-09-10 01:35:29 +0200

    adding references

--- apps/main/src/content/docs/Derivations/Displays, Light and Vision.ts
+++ apps/main/src/content/docs/Derivations/Displays, Light and Vision.ts
@@ -10,6 +10,7 @@ export const references = [
   ...catalog.filter(m => m["collection-title"]?.includes('CS 184') && m.issued?.["date-parts"]?.[0]?.[0] === 2026 && m.issued?.season === 2),
   ...catalog.filter(m => /光学/.test(m.title!) && /新概念物理教程/.test(m.title!)),
   catalog.get('Rapport BIPM-2019/05'),
-  ...catalog.filter(m => /Mise en pratique for the definition of the candela/.test(m.title!))
+  ...catalog.filter(m => /Mise en pratique for the definition of the candela/.test(m.title!)),
+  ...catalog.filter(m => util.ieq(m['container-title']!, 'E-ILV'))
 ]
 
---
+++ apps/main/src/data/materials/CIE S 017-E-2020.ts
@@ -0,0 +1,99 @@
+import * as types_data from '@cs-first-aid/bibkit/types/data'
+
+export const entries = [
+  {
+    id: [
+      'CIE 17-21-060',
+      'illuminance',
+    ],
+    material: {
+      type: 'entry-dictionary',
+      title: 'illuminance',
+      author: [ { literal: 'International Commission on Illumination', }, ],
+      'container-title': 'E-ILV',
+      publisher: 'International Commission on Illumination',
+      'publisher-place': 'Vienna',
+      number: '17-21-060',
+      issued: { 'date-parts': [ [ 2020, 12 ] ] },
+      accessed: { 'date-parts': [ [ 2026, 9, 9 ] ] },
+      language: 'en-GB',
+      URL: 'https://cie.co.at/eilvterm/17-21-060',
+    },
+  },
+  {
+    id: [
+      'CIE 17-21-080',
+      'radiant exitance',
+    ],
+    material: {
+      type: 'entry-dictionary',
+      title: 'radiant exitance',
+      'container-title': 'E-ILV',
+      author: [ { literal: 'International Commission on Illumination' } ],
+      publisher: 'International Commission on Illumination',
+      'publisher-place': 'Vienna',
+      number: '17-21-080',
+      issued: { 'date-parts': [ [ 2020, 12 ] ] },
+      language: 'en-GB',
+      URL: 'https://cie.co.at/eilvterm/17-21-080',
+      accessed: { 'date-parts': [ [ 2026, 9, 9 ] ] },
+    },
+  },
+  {
+    id: [
+      'CIE 17-21-081',
+      'luminous exitance',
+    ],
+    material: {
+      type: 'entry-dictionary',
+      title: 'luminous exitance',
+      'container-title': 'E-ILV',
+      author: [ { literal: 'International Commission on Illumination' } ],
+      publisher: 'International Commission on Illumination',
+      'publisher-place': 'Vienna',
+      number: '17-21-081',
+      issued: { 'date-parts': [ [ 2020, 12 ] ] },
+      language: 'en-GB',
+      URL: 'https://cie.co.at/eilvterm/17-21-081',
+      accessed: { 'date-parts': [ [ 2026, 9, 9 ] ] },
+    },
+  },
+  {
+    id: [
+      'CIE 17-24-062',
+      'Lambert\'s cosine law',
+    ],
+    material: {
+      type: 'entry-dictionary',
+      title: 'Lambert\'s cosine law',
+      'container-title': 'E-ILV',
+      author: [ { literal: 'International Commission on Illumination' } ],
+      publisher: 'International Commission on Illumination',
+      'publisher-place': 'Vienna',
+      number: '17-24-062',
+      issued: { 'date-parts': [ [ 2020, 12 ] ] },
+      language: 'en-GB',
+      URL: 'https://cie.co.at/eilvterm/17-24-062',
+      accessed: { 'date-parts': [ [ 2026, 9, 9 ] ] },
+    },
+  },
+  {
+    id: [
+      'CIE 17-24-063',
+      'Lambertian surface',
+    ],
+    material: {
+      type: 'entry-dictionary',
+      title: 'Lambertian surface',
+      'container-title': 'E-ILV',
+      author: [ { literal: 'International Commission on Illumination' } ],
+      publisher: 'International Commission on Illumination',
+      'publisher-place': 'Vienna',
+      number: '17-24-063',
+      issued: { 'date-parts': [ [ 2020, 12 ] ] },
+      language: 'en-GB',
+      URL: 'https://cie.co.at/eilvterm/17-24-063',
+      accessed: { 'date-parts': [ [ 2026, 9, 9 ] ] },
+    },
+  },
+] satisfies types_data.Entry[]
