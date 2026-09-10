88abd30365f9adde091d0a83ba862f2543bc172a
AndyBRoswellAgent <andyroswell-a-g-e-n-t@hotmail.com>
2026-09-10 19:56:31 +0200

    Update BIPM, PRINCIPLES GOVERNING PHOTOMETRY.ts

--- apps/main/src/data/materials/BIPM, PRINCIPLES GOVERNING PHOTOMETRY.ts
+++ apps/main/src/data/materials/BIPM, PRINCIPLES GOVERNING PHOTOMETRY.ts
@@ -1,4 +1,5 @@
 import * as types_data from '@cs-first-aid/bibkit/types/data'
+import * as CSL from '@cs-first-aid/bibkit/CSL'
 
 export const entries = [
   {
@@ -16,8 +17,17 @@ export const entries = [
       'publisher-place': 'Sèvres, France',
       issued: { 'date-parts': [ [ 2019, 4, 14 ] ] },
       language: 'en-GB',
-      URL: 'https://www.bipm.org/documents/20126/41749107/Principles%20governing%20photometry/8365350c-bf72-3cf2-bf03-324bf5c01b62',
-      accessed: { 'date-parts': [ [ 2026, 9, 9 ] ] },
+      URL: 'https://www.bipm.org/en/publications/mises-en-pratique/',
+      accessed: { 'date-parts': [ [ 2026, 9, 10 ] ] },
+      custom: {
+        free_material: [
+          {
+            link: 'https://www.bipm.org/documents/20126/41749107/Principles%20governing%20photometry/8365350c-bf72-3cf2-bf03-324bf5c01b62',
+            display_text: 'PDF',
+            'Content-Type': 'application/pdf',
+          },
+        ],
+      } satisfies CSL.Custom,
     },
   },
 ] satisfies types_data.Entry[]
