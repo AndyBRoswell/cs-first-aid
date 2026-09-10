0840784fa61ea88ee39164c6d4a6a26cc366c8ed
AndyBRoswellAgent <andyroswell-a-g-e-n-t@hotmail.com>
2026-09-10 20:00:01 +0200

    Update S. Axler, Linear Algebra Done Right.ts

--- apps/main/src/data/materials/S. Axler, Linear Algebra Done Right.ts
+++ apps/main/src/data/materials/S. Axler, Linear Algebra Done Right.ts
@@ -1,4 +1,5 @@
 import * as types_data from '@cs-first-aid/bibkit/types/data'
+import * as CSL from '@cs-first-aid/bibkit/CSL'
 
 export const entries = [
   {
@@ -10,11 +11,21 @@ export const entries = [
       author: [ { given: 'Sheldon', family: 'Axler' } ],
       title: 'Linear Algebra Done Right',
       edition: 4,
-      issued: { 'date-parts': [ [ 2026, 4, 26 ] ] },
+      issued: { 'date-parts': [ [ 2026, 8, 16 ] ] },
       publisher: 'Springer',
       language: 'en-US',
-      URL: 'https://linear.axler.net/LADR4e.pdf',
-      accessed: { 'date-parts': [ [ 2026, 5, 5 ] ] },
+      URL: 'https://linear.axler.net/index.html',
+      accessed: { 'date-parts': [ [ 2026, 9, 10 ] ] },
+      custom: {
+        free_material: [
+          {
+            link: 'https://linear.axler.net/LADR4e.pdf',
+            display_text: 'PDF',
+            'Content-Type': 'application/pdf',
+            license: 'CC-BY-NC-4.0',
+          },
+        ],
+      } satisfies CSL.Custom,
     },
   },
 ] satisfies types_data.Entry[]
