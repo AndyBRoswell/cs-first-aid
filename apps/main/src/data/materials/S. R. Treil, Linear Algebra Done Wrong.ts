ef49c003a3d3212d5d2649c6c303871e2a922250
AndyBRoswellAgent <andyroswell-a-g-e-n-t@hotmail.com>
2026-09-10 20:06:36 +0200

    Update S. R. Treil, Linear Algebra Done Wrong.ts

--- apps/main/src/data/materials/S. R. Treil, Linear Algebra Done Wrong.ts
+++ apps/main/src/data/materials/S. R. Treil, Linear Algebra Done Wrong.ts
@@ -11,14 +11,26 @@ export const entries = [
       author: [ { given: 'Sergei Raimondovich', family: 'Treil' } ],
       title: 'Linear Algebra Done Wrong',
       language: 'en-US',
-      issued: { "date-parts": [ [ 2026, 4, 30 ] ] },
-      URL: 'https://www.math.brown.edu/streil/papers/LADW/LADW.html',
+      issued: { "date-parts": [ [ 2026, 8, 31 ] ] },
+      URL: 'https://sites.google.com/a/brown.edu/sergei-treil-homepage/linear-algebra-done-wrong',
       accessed: { 'date-parts': [ [ 2026, 9, 10 ] ] },
       custom: {
         free_material: [
           {
-            link: 'https://www.math.brown.edu/streil/papers/LADW/HTML_2026_04-30/LADW_2026_04-30.html',
+            link: 'https://www.math.brown.edu/streil/papers/LADW/HTML_2026_08-31/LADW_2026_08-31.html',
             display_text: 'HTML',
+            license: 'CC-BY-NC-ND-3.0',
+          },
+          {
+            link: 'https://www.math.brown.edu/~treil/papers/LADW/LADW_2026_08-31.pdf',
+            display_text: 'PDF',
+            'Content-Type': 'application/pdf',
+            license: 'CC-BY-NC-ND-3.0',
+          },
+          {
+            link: 'https://www.math.brown.edu/~treil/papers/LADW/LADW_2026_08-31-cropped.pdf',
+            display_text: 'PDF (cropped)',
+            'Content-Type': 'application/pdf',
             license: 'CC-BY-NC-ND-3.0',
           },
         ],
