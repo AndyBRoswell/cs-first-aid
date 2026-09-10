17eee195336620f6729bfa57d2e734e1975caa07
AndyBRoswellAgent <andyroswell-a-g-e-n-t@hotmail.com>
2026-09-10 20:00:29 +0200

    Update S. R. Treil, Linear Algebra Done Wrong.ts

--- apps/main/src/data/materials/S. R. Treil, Linear Algebra Done Wrong.ts
+++ apps/main/src/data/materials/S. R. Treil, Linear Algebra Done Wrong.ts
@@ -1,4 +1,5 @@
 import * as types_data from '@cs-first-aid/bibkit/types/data'
+import * as CSL from '@cs-first-aid/bibkit/CSL'
 
 export const entries = [
   {
@@ -11,8 +12,17 @@ export const entries = [
       title: 'Linear Algebra Done Wrong',
       language: 'en-US',
       issued: { "date-parts": [ [ 2026, 4, 30 ] ] },
-      URL: 'https://www.math.brown.edu/streil/papers/LADW/HTML_2026_04-30/LADW_2026_04-30.html',
-      accessed: { 'date-parts': [ [ 2026, 5, 5 ] ] },
+      URL: 'https://www.math.brown.edu/streil/papers/LADW/LADW.html',
+      accessed: { 'date-parts': [ [ 2026, 9, 10 ] ] },
+      custom: {
+        free_material: [
+          {
+            link: 'https://www.math.brown.edu/streil/papers/LADW/HTML_2026_04-30/LADW_2026_04-30.html',
+            display_text: 'HTML',
+            license: 'CC-BY-NC-ND-3.0',
+          },
+        ],
+      } satisfies CSL.Custom,
     },
   },
 ] satisfies types_data.Entry[]
