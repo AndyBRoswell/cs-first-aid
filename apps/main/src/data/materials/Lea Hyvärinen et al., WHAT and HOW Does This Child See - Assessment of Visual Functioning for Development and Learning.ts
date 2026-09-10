52dca7cf5f441ef53c05a3487ed3de7c6f4614e2
AndyBRoswellAgent <andyroswell-a-g-e-n-t@hotmail.com>
2026-09-10 20:04:27 +0200

    Update Lea Hyvärinen et al., WHAT and HOW Does This Child See - Assessment of Visual Functioning for Development and Learning.ts

--- apps/main/src/data/materials/Lea Hyvärinen et al., WHAT and HOW Does This Child See - Assessment of Visual Functioning for Development and Learning.ts
+++ apps/main/src/data/materials/Lea Hyvärinen et al., WHAT and HOW Does This Child See - Assessment of Visual Functioning for Development and Learning.ts
@@ -1,4 +1,5 @@
 import * as types_data from '@cs-first-aid/bibkit/types/data'
+import * as CSL from '@cs-first-aid/bibkit/CSL'
 
 export const entries = [
   {
@@ -8,11 +9,20 @@ export const entries = [
       title: 'WHAT and HOW Does This Child See? Assessment of Visual Functioning for Development and Learning',
       author: [ { given: 'Lea', family: 'Hyvärinen' }, { given: 'Namita', family: 'Jacob' } ],
       issued: { 'date-parts': [ [ 2011 ] ] },
-      accessed: { 'date-parts': [ [ 2026, 8, 20 ] ] },
-      URL: 'http://www.lea-test.fi/en/vistests/instruct/contrast/lowsymbo/Snellen.pdf',
+      accessed: { 'date-parts': [ [ 2026, 9, 10 ] ] },
+      URL: 'https://books.google.com/books/about/What_and_how_Does_this_Child_See.html?id=HDFrtwAACAAJ',
       ISBN: '9789529283804',
       language: 'en-US',
       "number-of-pages": 238,
+      custom: {
+        free_material: [
+          {
+            link: 'http://www.lea-test.fi/en/vistests/instruct/contrast/lowsymbo/Snellen.pdf',
+            display_text: 'Visual Acuity Conversion Chart',
+            'Content-Type': 'application/pdf',
+          },
+        ],
+      } satisfies CSL.Custom,
     },
   } satisfies types_data.Entry,
 ] satisfies types_data.Entry[]
