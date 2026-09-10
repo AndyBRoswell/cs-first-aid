09d2c02cbeb8d9d144b68d62242b43d79c3e58f2
AndyBRoswellAgent <andyroswell-a-g-e-n-t@hotmail.com>
2026-09-10 20:01:35 +0200

    Update V. C. Smith & J. Pokorny, Color Matching and Color Discrimination.ts

--- apps/main/src/data/materials/V. C. Smith & J. Pokorny, Color Matching and Color Discrimination.ts
+++ apps/main/src/data/materials/V. C. Smith & J. Pokorny, Color Matching and Color Discrimination.ts
@@ -16,10 +16,18 @@ export const entries = [
       edition: 2,
       issued: { 'date-parts': [ [ 2003, 7, 11 ] ] },
       ISBN: '9780080523224',
+      DOI: '10.1016/B978-044451251-2/50004-0',
       language: 'en-US',
-      URL: 'https://sites.socsci.uci.edu/~kjameson/SmithPokorny2003.pdf',
-      accessed: { 'date-parts': [ [ 2026, 8, 23 ] ] },
+      URL: 'https://www.sciencedirect.com/science/article/pii/B9780444512512500040',
+      accessed: { 'date-parts': [ [ 2026, 9, 10 ] ] },
       custom: {
+        free_material: [
+          {
+            link: 'https://sites.socsci.uci.edu/~kjameson/SmithPokorny2003.pdf',
+            display_text: '3 Color Matching and Color Discrimination',
+            'Content-Type': 'application/pdf',
+          },
+        ],
         variant: [
           {
             type: 'book',
