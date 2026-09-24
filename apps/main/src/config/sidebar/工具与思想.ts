ccfd7c7ccbe350f036ed1b143861fbccab7e51a2
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2026-09-24 22:01:27 +0200

    rev：用钱换时间

--- apps/main/src/config/sidebar/工具与思想.ts
+++ apps/main/src/config/sidebar/工具与思想.ts
@@ -142,7 +142,7 @@ export const Tools_and_Ideas = {
       },
       slug: 'tools-and-ideas/save-time-with-money',
       attrs: {
-        'data-release-stage': release_stages.to_HTML_attr({ 'zh-CN': '2026.1.0-beta', en: 'blank', })
+        'data-release-stage': release_stages.to_HTML_attr({ 'zh-CN': '2026.1.0-rc', en: 'blank', })
       },
     },
     {
