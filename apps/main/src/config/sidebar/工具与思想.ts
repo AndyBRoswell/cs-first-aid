93b192fea96cdcb1585680832a66eead68c010d3
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2026-09-24 19:41:36 +0200

    rev：自学

--- apps/main/src/config/sidebar/工具与思想.ts
+++ apps/main/src/config/sidebar/工具与思想.ts
@@ -18,11 +18,11 @@ export const Tools_and_Ideas = {
       },
     },
     {
-      label: '养成自学的习惯',
+      label: '自学',
       translations: {
-        en: 'Forming the Habit of Self-Learning',
+        en: 'Self-Learning',
       },
-      slug: 'tools-and-ideas/forming-the-habit-of-self-learning',
+      slug: 'tools-and-ideas/self-learning',
       attrs: {
         'data-release-stage': release_stages.to_HTML_attr({ 'zh-CN': '2026.1.0-alpha', en: 'blank', })
       },
