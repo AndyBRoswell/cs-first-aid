b519b3451c17e4d4c419bb6dc837d42e03a9bed9
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2026-09-24 18:42:36 +0200

    adding references

---
+++ apps/main/src/content/docs/Preface/Which Majors Are Considered Computer Science.ts
@@ -0,0 +1,7 @@
+import * as catalog from '@cs-first-aid/bibkit/catalog'
+import * as util from '@cs-first-aid/util'
+import '@/data/materials/import materials.ts'
+
+export const references = [
+  catalog.get('普通高等学校本科专业目录（2026年）'),
+]
